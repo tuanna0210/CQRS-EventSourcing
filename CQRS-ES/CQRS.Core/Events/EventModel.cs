abd0f855711bbd51e626bd4a5b860735063b3d43
Tuan Nguyen <anhtuann0210@gmail.com>
2023-11-14 23:00:31 +0700

    6.27. EventModel

---
+++ CQRS-ES/CQRS.Core/Events/EventModel.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Events
+{
+    public class EventModel
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public Guid AggregateIdentifier { get; set; }
+        public string AggregateType { get; set; }
+        public int Version { get; set; }
+        public string EventType { get; set; }
+        public BaseEvent EventData { get; set; }
+    }
+}
