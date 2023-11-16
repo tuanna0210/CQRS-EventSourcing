67a9ea4c94a0150954e721061860026c38ca0787
Tuan Nguyen <anhtuann0210@gmail.com>
2023-11-16 23:16:20 +0700

    11.52. EventHandler

--- SM-Post/Post.Query/Post.Query.API/Program.cs
+++ SM-Post/Post.Query/Post.Query.API/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Handlers;
+using Post.Query.Infrastructure.Repositories;
 
 namespace Post.Query.API
 {
@@ -9,15 +12,19 @@ namespace Post.Query.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            var a = builder.Configuration.GetConnectionString("SqlServer");
             // Add services to the container.
             Action<DbContextOptionsBuilder> configureDbContext = (o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
             builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
             builder.Services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
+            
 
             //Create database and tables from code
             var dbContext = builder.Services.BuildServiceProvider().GetService<DatabaseContext>();
             dbContext.Database.EnsureCreated();
+
+            builder.Services.AddScoped<IPostRepository, PostRepository>();
+            builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+            builder.Services.AddScoped<IEventHandler, Infrastructure.Handlers.EventHandler>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
