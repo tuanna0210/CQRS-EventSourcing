089b2c228b0e84aa9102756a8064d91c3070bb36
Tuan Nguyen <anhtuann0210@gmail.com>
2023-11-17 12:17:05 +0700

    12.56. EventConsumer

--- SM-Post/Post.Query/Post.Query.API/Program.cs
+++ SM-Post/Post.Query/Post.Query.API/Program.cs
@@ -1,5 +1,8 @@
+using Confluent.Kafka;
+using CQRS.Core.Consumers;
 using Microsoft.EntityFrameworkCore;
 using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
@@ -25,6 +28,8 @@ namespace Post.Query.API
             builder.Services.AddScoped<IPostRepository, PostRepository>();
             builder.Services.AddScoped<ICommentRepository, CommentRepository>();
             builder.Services.AddScoped<IEventHandler, Infrastructure.Handlers.EventHandler>();
+            builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
+            builder.Services.AddScoped<IEventConsumer, EventConsumer>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
