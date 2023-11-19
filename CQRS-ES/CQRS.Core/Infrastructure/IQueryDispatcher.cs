34d5c818146f29c93884d80def8acc30d5158d12
Tuan Nguyen <anhtuann0210@gmail.com>
2023-11-19 22:16:13 +0700

    14.76. IQueryDispatcher interface

---
+++ CQRS-ES/CQRS.Core/Infrastructure/IQueryDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using CQRS.Core.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Infrastructure
+{
+    public interface IQueryDispatcher<TEntity>
+    {
+        void RegisterHandler<TQuery>(Func<TQuery, List<TEntity>> handler) where TQuery: BaseQuery;
+        Task<List<TEntity>> SendAsync(BaseQuery query);
+    }
+}
