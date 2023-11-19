82cfb5362b8c51f87f78f19a79a4410374ecd0d8
Tuan Nguyen <anhtuann0210@gmail.com>
2023-11-19 22:25:06 +0700

    14.77. QueryDispatcher concrete implementation

--- CQRS-ES/CQRS.Core/Infrastructure/IQueryDispatcher.cs
+++ CQRS-ES/CQRS.Core/Infrastructure/IQueryDispatcher.cs
@@ -9,7 +9,7 @@ namespace CQRS.Core.Infrastructure
 {
     public interface IQueryDispatcher<TEntity>
     {
-        void RegisterHandler<TQuery>(Func<TQuery, List<TEntity>> handler) where TQuery: BaseQuery;
+        void RegisterHandler<TQuery>(Func<TQuery, Task<List<TEntity>>> handler) where TQuery: BaseQuery;
         Task<List<TEntity>> SendAsync(BaseQuery query);
     }
 }
