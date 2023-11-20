594b31c20d0f52f2c9f62cbc5be2260a9bfc4dad
Tuan Nguyen <anhtuann0210@gmail.com>
2023-11-20 14:41:29 +0700

    15.81. Register query handlers

--- SM-Post/Post.Query/Post.Query.API/Program.cs
+++ SM-Post/Post.Query/Post.Query.API/Program.cs
@@ -1,9 +1,13 @@
 using Confluent.Kafka;
 using CQRS.Core.Consumers;
+using CQRS.Core.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Post.Query.API.Queries;
+using Post.Query.Domain.Entities;
 using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Dispatchers;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
 
@@ -27,9 +31,20 @@ namespace Post.Query.API
 
             builder.Services.AddScoped<IPostRepository, PostRepository>();
             builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+            builder.Services.AddScoped<IQueryHandler, QueryHandler>();
             builder.Services.AddScoped<IEventHandler, Infrastructure.Handlers.EventHandler>();
             builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
             builder.Services.AddScoped<IEventConsumer, EventConsumer>();
+
+            //Register query handler methods
+            var queryHandler= builder.Services.BuildServiceProvider().GetRequiredService<IQueryHandler>();
+            var dispatcher = new QueryDispatcher();
+            dispatcher.RegisterHandler<FindAllPostsQuery>(queryHandler.HandleAsync);
+            dispatcher.RegisterHandler<FindPostByIdQuery>(queryHandler.HandleAsync);
+            dispatcher.RegisterHandler<FindPostsByAuthorQuery>(queryHandler.HandleAsync);
+            dispatcher.RegisterHandler<FindPostsWithCommentsQuery>(queryHandler.HandleAsync);
+            dispatcher.RegisterHandler<FindPostsWithLikesQuery>(queryHandler.HandleAsync);
+            builder.Services.AddSingleton<IQueryDispatcher<PostEntity>>(_=> dispatcher);
 
             builder.Services.AddHostedService<ConsumerHostedService>();
             builder.Services.AddControllers();
